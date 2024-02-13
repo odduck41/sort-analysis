6d250d71a3a84046242cc189ab799e583b369dd5
odduck41 <GD.cats.creators@gmail.com>
2024-02-14 01:02:23 +0300

    researching

--- main.cpp
+++ main.cpp
@@ -5,6 +5,21 @@
 // first =, second <=>
 
 int main() {
-    int length[10] = {7, 41, 43, 569,439, 4013, 8179, 13441, 78929, 3500443};
-
+    std::freopen("data.txt", "w", stdout);
+    int length[10] = {5,7, 41, 43, 439, 569, 4013, 8179, 13441, 21023};
+    for (auto& l : length) {
+        std::cout << "length " << l << ":\n";
+        auto assign = 0ull;
+        auto compare = 0ull;
+        for (int i = 0; i < 7; ++i) {
+            auto g = Generator();
+            auto v = g(l);
+            std::pair<unsigned long long, unsigned long long> operations = {0, 0};
+            selection_sort(operations, v);
+            assign += operations.first;
+            compare += operations.second;
+        }
+        std::cout << "\tselection sort:\n" << "\t\tassign = " << assign / 7<< "\n" << "\t\tcompare = " << compare / 7;
+        std::cout << "\n";
+    }
 }
